559ef86d4533def9153c42b23b56d41c4e1e76a3
ZL100702 <zakaria.lamdaghri@ynov.com>
2025-06-30 14:15:14 +0200

    Update SimpleMath.py

--- SimpleMath.py
+++ SimpleMath.py
@@ -7,16 +7,4 @@ class SimpleMath:
     def addition(a, b):
         """Additionne deux nombres."""
         return a + b
- test_simple_math.py
-python
-Copier
-Modifier
-import unittest
-from simple_math import SimpleMath
 
-class TestSimpleMath(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(SimpleMath.addition(2, 3), 5)
-
-if __name__ == '__main__':
-    unittest.main()
